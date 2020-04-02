cf712eaa3adce0f4e992ab21d8d4892cef61b57a
diogo <diogo.libras43@gmail.com>
2020-04-02 10:00:33 -0300

    finalizado o frontend do app

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import logoImg from '../../assets/img/logo.svg';
+import api from '../../services/api';
 
-
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {FiArrowDownLeft} from 'react-icons/fi';
 
 export default function NewIncident(){
+    const [title, setTitle] = useState();
+    const [description, setDescription] = useState();
+    const [value, setValue] = useState();
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
+    async function hadleNewIncident(e){
+        e.preventDefault();
+        const data = {
+            title, description, value
+        };
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId 
+                }
+            })
+            history.push('/profile');
+        } catch (error) {
+            alert('erro no cadastro de casos, tente novamente');
+        }
+
+    }
     return (
         <div className="new-incident-container">
         <div className="content">
@@ -21,10 +44,22 @@ export default function NewIncident(){
                         Voltar para home
                      </Link>
             </section>
-            <form action="">
-                <input type="text" name="" id="" placeholder="título do caso"/>
-                <textarea type="email" name="" id="" placeholder="descrição"/>
-                <input type="" placeholder="valor em reais"/>
+            <form action="" onSubmit={hadleNewIncident}>
+                <input type="text" name="" id=""
+                     placeholder="título do caso"
+                     value={title}
+                     onChange={e => setTitle(e.target.value)}
+                     />
+                <textarea type="email" name="" id=""
+                     placeholder="descrição"
+                     value={description}
+                     onChange={e => setDescription(e.target.value)}
+                     />
+                <input type=""
+                     placeholder="valor em reais"
+                     value={value}
+                     onChange={e => setValue(e.target.value)}
+                     />
 
                     <button className="button" type="submit"> Cadastrar</button>
 
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {FiPower, FiTrash} from 'react-icons/fi';
 import './style.css';
 import api from '../../services/api';
@@ -10,7 +10,7 @@ export default function Profile(){
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
     
-    
+    const history = useHistory();
     useEffect(() => {
         api.get('profile', {
             headers : { Authorization: ongId}})
@@ -19,6 +19,25 @@ export default function Profile(){
                 
             })
     }, [ongId]);
+    async function handleDeleteIncident(id){
+        try {
+          await api.delete(`incidents/${id}`,{
+              headers: {
+                  Authorization: ongId
+              }
+          })  
+        } catch (error) {
+            alert("erro ao deletar o caso, tente novamente");
+        }
+
+
+        setIncidents(incidents.filter (incident => incident.id !== id));
+    }
+
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
 
 
     return (
@@ -38,7 +57,7 @@ export default function Profile(){
                     Cadastrar novo caso
                 </Link>
 
-                <button type="submit">
+                <button type="submit"  onClick={handleLogout}>
 
                         <FiPower size={18} color="#e02041"/>
 
@@ -55,9 +74,9 @@ export default function Profile(){
                         <strong>DESCRIÇÃO:</strong>
                         <p>{incident.description}</p>
                         
-                        <strong>VALOR::</strong>
-                            <p>{incident.value}</p>
-                                <button >
+                        <strong>VALOR:</strong>
+                            <p>{Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
+                                <button onClick={() => handleDeleteIncident(incident.id)} >
                                     <FiTrash size={20} color="#a8a8b3" />
                                 </button>
                     </li>
